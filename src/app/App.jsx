0b2fdcf54ebc11942ccbf86ee87f59502d03deae
Paul van Mierlo <pauljozefvanmierlo@yahoo.com>
2020-12-30 16:38:52 +0100

    comment verwijderd en functie gemaakt

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -7,30 +7,21 @@ import RepresentationMultipleStudents from "../representation-multiple-students/
 import RepresentationMultipleAssignments from "../representation-multiple-assignments/representationMultipleAssignments";
 import "./App.css";
 
-/*
-async function scoresFetch(scores) {
-  try {
-    const response = await 
-    const scoresData = await response.text();
-    await console.log(scoresData);
-    const scoreLines = scoresData.split("\n");
-    console.log("aantal scores : " + scoreLines.length);
-    scoreLines.forEach((score) => {
-      const scoreData = score.split(",");
-      scores.push({
-        student: scoreData[0],
-        assignment: scoreData[1],
-        difficultGrade: scoreData[2],
-        niceGrade: scoreData[3],
-      });
-    });
-  } catch (err) {
-    console.log(err);
-  }
-}
-*/
-
 let scores = [];
+
+const addScore = (score) => {
+  const scoreData = score.split(",");
+  if (scoreData.length === 4) {
+    const student = scoreData[0].replaceAll('"', "");
+    const assignment = scoreData[1].replaceAll('"', "");
+    scores.push({
+      student,
+      assignment,
+      difficultGrade: parseInt(scoreData[2].trim()),
+      niceGrade: parseInt(scoreData[3].trim()),
+    });
+  }
+};
 
 class App extends Component {
   constructor() {
@@ -43,19 +34,7 @@ class App extends Component {
       .then((response) => response.text())
       .then((scoresData) => {
         const scoreLines = scoresData.split("\n");
-        scoreLines.forEach((score) => {
-          const scoreData = score.split(",");
-          if (scoreData.length === 4) {
-            const student = scoreData[0].replaceAll('"', "");
-            const assignment = scoreData[1].replaceAll('"', "");
-            scores.push({
-              student,
-              assignment,
-              difficultGrade: parseInt(scoreData[2].trim()),
-              niceGrade: parseInt(scoreData[3].trim()),
-            });
-          }
-        });
+        scoreLines.forEach((score) => addScore(score));
         this.setState({ scores, schermGeladen: true });
       });
   }
